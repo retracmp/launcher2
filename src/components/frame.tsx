c2494a100606a75d649db477365db6f491fcea5c
Eccentric <eccentric@retrac.site>
2025-02-25 20:43:38 +0000

    start impementing frame

--- src/components/frame.tsx
+++ src/components/frame.tsx
@@ -1,4 +1,13 @@
-import { IoCloseSharp, IoArrowBackSharp } from "react-icons/io5";
+import { getCurrentWindow } from "@tauri-apps/api/window";
+
+import {
+  IoCloseSharp,
+  IoArrowBackSharp,
+  IoHomeSharp,
+  IoDownloadSharp,
+  IoFileTrayFullSharp,
+  IoStatsChartSharp,
+} from "react-icons/io5";
 
 const Frame = () => {
   return (
@@ -6,7 +15,26 @@ const Frame = () => {
       className="flex flex-row w-full h-full bg-neutral-900"
       data-tauri-drag-region
     >
-      <nav className="flex h-full w-15 border-r-[#2e2e2e] border-r-1 border-solid"></nav>
+      <nav className="flex flex-col items-center gap-1 p-1.5 h-full w-12 border-r-[#2e2e2e] border-r-1 border-solid">
+        <button className="flex items-center justify-center w-9 h-9 cursor-pointer hover:bg-[#1f1f1f3f] rounded-xs">
+          <IoHomeSharp className="text-neutral-400" />
+        </button>
+
+        {/* selected */}
+        <button className="flex items-center justify-center w-9 h-9 cursor-pointer bg-[#1f1f1f] bg-opacity-50 rounded-xs border-[#2e2e2e] border-1 border-solid">
+          <IoDownloadSharp className="text-neutral-300" />
+        </button>
+
+        <button className="flex items-center justify-center w-9 h-9 cursor-pointer hover:bg-[#1f1f1f3f] rounded-xs">
+          <IoFileTrayFullSharp className="text-neutral-400" />
+        </button>
+
+        {/* selected */}
+        <button className="flex items-center justify-center w-9 h-9 cursor-pointer">
+          <IoStatsChartSharp className="text-neutral-400" />
+        </button>
+      </nav>
+
       <div className="flex flex-1 flex-col">
         <nav
           className="flex items-center pl-1.5 w-full bg-[#191919] h-7 border-b-[#2e2e2e] border-b-1 border-solid"
@@ -19,10 +47,17 @@ const Frame = () => {
             Retrac
           </p>
           <s className="ml-auto" />
-          <button className="flex items-center justify-center w-9 h-full cursor-pointer hover:bg-[#2e2e2e] hover:bg-opacity-50">
+
+          <button
+            className="flex items-center justify-center w-9 h-full cursor-pointer hover:bg-[#2e2e2e] hover:bg-opacity-50"
+            onClick={() => history.back()}
+          >
             <IoArrowBackSharp className="text-neutral-400 w-[14px] h-[14px]" />
           </button>
-          <button className="flex items-center justify-center w-9 h-full cursor-pointer hover:bg-[#2e2e2e] hover:bg-opacity-50">
+          <button
+            className="flex items-center justify-center w-9 h-full cursor-pointer hover:bg-[#2e2e2e] hover:bg-opacity-50"
+            onClick={() => getCurrentWindow().close()}
+          >
             <IoCloseSharp className="text-neutral-400" />
           </button>
         </nav>
