cb536f43fa841709aa00690fb034273a79a117ce
Eccentric <eccentric@retrac.site>
2025-03-19 22:49:35 +0000

    finish lootlabs widget

--- src/axios/client.ts
+++ src/axios/client.ts
@@ -29,4 +29,30 @@ const get_discord_login_url = async (): Promise<RResponse<string>> => {
   };
 };
 
-export default { get_discord_login_url };
+const get_lootlabs_offer_url = async (
+  access_token: string
+): Promise<RResponse<string>> => {
+  const response = await axiosClient
+    .get<string>("/adverts", {
+      headers: {
+        Authorization: access_token,
+      },
+    })
+    .catch((e: AxiosError<any>) => {
+      return e;
+    });
+
+  if (response instanceof AxiosError) {
+    return {
+      ok: false,
+      error: response.message,
+    };
+  }
+
+  return {
+    ok: true,
+    data: response.data,
+  };
+};
+
+export default { get_discord_login_url, get_lootlabs_offer_url };
--- src/components/routes/app/home/widgets/lootlabs.tsx
+++ src/components/routes/app/home/widgets/lootlabs.tsx
@@ -1,7 +1,41 @@
-import { MutableRefObject, useEffect, useRef } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
+import { useUserManager } from "src/wrapper/user";
+import { openUrl } from "@tauri-apps/plugin-opener";
+import client from "src/axios/client";
+
 import UI from "src/components/core/default";
+import { formatTime } from "src/helpers/time";
 
 const LootLabsWidget = () => {
+  const user = useUserManager();
+  if (user._user === null) return null;
+
+  const claimed = user._user.Account.State.ClaimedPackages["lootlabs_1kvbucks"];
+  const difference = new Date().getTime() - new Date(claimed || 0).getTime();
+  const disabled = difference < 24 * 60 * 60 * 1000;
+
+  const [originalText, setOriginalText] = useState(
+    formatTime(24 * 60 * 60 * 1000 - difference)
+  );
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setOriginalText(formatTime(24 * 60 * 60 * 1000 - difference));
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [difference]);
+
+  const handleClaimOffer = async () => {
+    if (user._token === null)
+      return console.error("No token found, so cannot claim the link");
+
+    const link = await client.get_lootlabs_offer_url(user._token);
+    if (link.ok) {
+      openUrl(link.data);
+    }
+  };
+
   return (
     <div className="relative flex flex-col p-2 gap-0.5 w-[70%] bg-neutral-800/10 rounded-xs border-[#2e2e2e] border-1 border-solid">
       <UI.H1 className="z-20">Looking for more?</UI.H1>
@@ -22,10 +56,18 @@ const LootLabsWidget = () => {
 
       <UI.Button
         colour="neutral"
-        className="p-1.5 mt-auto z-10 backdrop-blur-2xl"
+        className="p-1.5 mt-auto backdrop-blur-2xl z-20 pointer-events-auto"
+        onClick={handleClaimOffer}
+        disabled={disabled}
       >
         <UI.P className="text-neutral-300">
-          <span className="font-[500]">Claim Now</span>
+          {!disabled ? (
+            <span className="font-[500]">Claim your reward now!</span>
+          ) : (
+            <span className="font-[500]">
+              Come back in {originalText} to claim your reward!
+            </span>
+          )}
         </UI.P>
       </UI.Button>
     </div>
@@ -35,7 +77,7 @@ const LootLabsWidget = () => {
 const RainingEmojis = () => {
   return (
     <div className="absolute top-0 left-0 w-full h-full">
-      {[...Array(40)].map((_, idx) => (
+      {[...Array(20)].map((_, idx) => (
         <Emoji key={idx} />
       ))}
     </div>
