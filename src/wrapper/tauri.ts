e9dbe3ab00a27f88272016cc1b8fc212f7f4fe4e
Eccentric <eccentric@retrac.site>
2025-10-08 16:40:05 +0100

    fix bug with outdated launcher

--- src/wrapper/tauri.ts
+++ src/wrapper/tauri.ts
@@ -20,7 +20,7 @@ type ApplicationInformation = {
 export const useApplicationInformation = create<ApplicationInformation>()(
   (set) => ({
     name: "Retrac",
-    version: "2.0.7",
+    version: "2.0.13",
     dev: false,
     windowsVersion: 0,
     load: (name, version, dev, windowsVersion) =>
